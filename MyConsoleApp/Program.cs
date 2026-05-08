f2780541dd4e46c8e9ab886e3886362add13c90d
Madison Thomas <madison.thomas094@gmail.com>
2026-05-08 09:55:56 -0500

    Added .NET tutorial work

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is {DateTime.Now}");
+DateTime today = DateTime.Today;
+DateTime christmas = new DateTime(today.Year, 12, 25);
+if (christmas < today)
+{
+  christmas = new DateTime(today.Year + 1, 12, 25);
+}
+int daysUntil = (christmas - today).Days;
+Console.WriteLine($"There are {daysUntil} days until Christmas!");
